b45ffed160b75547b037f34978b4ac5d863a5f76
Your Name <you@example.com>
2026-02-15 15:30:46 -0500

    fixed our cors headers

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://products-bbby.onrender.com",
-        "https://chieng-products.vercel.app",
+        "https://chieng-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
